be0b9c88dc54a11cdd6d46ace687940054d1f55c
zobaer-zahad-reza <zobaerzahad.git@gmail.com>
2026-02-14 16:05:24 +0600

    added whatsapp buble icon

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,24 +5,27 @@ import Footer from "./Components/Footer";
 import ScrollToTop from "./Components/ScrollToTop";
 import SocialSideNav from "./Components/SocialSideNav";
 import ShopContextProvider from "./Context/ShopContext";
+import WhatsAppButton from "./Components/WhatsAppButton";
 
 function App() {
   return (
     <ShopContextProvider>
-    <div className="flex flex-col min-h-screen">
-      <div className="fixed top-0 left-0 w-full z-50">
-        <Navbar />
+      <div className="flex flex-col min-h-screen">
+        <div className="fixed top-0 left-0 w-full z-50">
+          <Navbar />
+        </div>
+
+        <ScrollToTop />
+        {/* <SocialSideNav /> */}
+
+        <WhatsAppButton />
+
+        <div className="min-h-screen pt-32">
+          <Outlet />
+        </div>
+
+        <Footer />
       </div>
-
-      <ScrollToTop />
-      {/* <SocialSideNav /> */}
-
-      <div className="min-h-screen pt-32">
-        <Outlet />
-      </div>
-
-      <Footer />
-    </div>
     </ShopContextProvider>
   );
 }
